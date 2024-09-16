89725d1ef8d17cff337f1a912d908fcc0ac1ce21
Deac <deacmanrbx@gmail.com>
2024-09-16 19:06:58 +0100

    3 or 4

---
+++ src/main/java/net/deac/customtextures/CustomTexturesClient.java
@@ -0,0 +1,11 @@
+package net.deac.customtextures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomTexturesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        //
+    }
+}
