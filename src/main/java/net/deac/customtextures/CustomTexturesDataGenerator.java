ede1e21a2608ab6e4a079d363073d76621527127
Deac <deacmanrbx@gmail.com>
2024-09-16 17:35:27 +0100

    wasd

---
+++ src/main/java/net/deac/customtextures/CustomTexturesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.deac.customtextures;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomTexturesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
